50981684550e8f2e5dd4349fb23d61772a4acc2c
Clear311 <44281330+Clear311@users.noreply.github.com>
2020-11-04 00:27:16 -0500

    updates

--- suzhou_web/image.js
+++ suzhou_web/image.js
@@ -1,3 +1,12 @@
 function showImage() {
     document.getElementById('first-image').style.visibility='visible';
+    document.getElementById('second-image').style.visibility='visible';
+    document.getElementById('third-image').style.visibility='visible';
+    document.getElementById('fourth-image').style.visibility='visible';
+    document.getElementById('fifth-image').style.visibility='visible';
+
 }
+
+// function showImage() {
+//     document.getElementById('second-image').style.visibility='visible';
+// }
