568337f3ac31bf4ea608dd73c4ee1f794df9fcc3
Clear311 <44281330+Clear311@users.noreply.github.com>
2020-12-01 15:28:51 -0500

    update

--- exquisite_creature/common/myscript.js
+++ exquisite_creature/common/myscript.js
@@ -87,21 +87,21 @@ $( function() {
     $( "#draggable4" ).draggable();
   } );
 
-$( function() {
-    $( "#resizable" ).resizable();
-  } );
+// $( function() {
+//     $( "#resizable" ).resizable();
+//   } );
 
-$( function() {
-    $( "#resizable2" ).resizable();
-  } );
+// $( function() {
+//     $( "#resizable2" ).resizable();
+//   } );
 
-$( function() {
-    $( "#resizable3" ).resizable();
-  } );
+// $( function() {
+//     $( "#resizable3" ).resizable();
+//   } );
 
-  $( function() {
-    $( "#resizable4" ).resizable();
-  } );
+//   $( function() {
+//     $( "#resizable4" ).resizable();
+//   } );
 
   function flip() {
     $('.card').toggleClass('flipped');
@@ -117,6 +117,73 @@ function flip3() {
 
 function flip4() {
     $('.card-4').toggleClass('flipped');
+}
+
+function flip_adj(str){
+    if(str == "Curious"){
+    document.getElementById('resizable').getElementsByTagName('img')[0].src='eyes/curious.png'
+    }
+    else if(str == "Tired"){
+    document.getElementById('resizable').getElementsByTagName('img')[0].src='eyes/tired-2.png'
+    }
+    else if(str == "Deadpan"){
+        document.getElementById('resizable').getElementsByTagName('img')[0].src='eyes/deadpan-2.png'
+    }
+    else if(str == "Delighted"){
+        document.getElementById('resizable').getElementsByTagName('img')[0].src='eyes/delighted.png'
+    }
+    else if(str == "Excited"){
+        document.getElementById('resizable').getElementsByTagName('img')[0].src='eyes/excited.png'
+    }
+    else if(str == "Fearful"){
+        document.getElementById('resizable').getElementsByTagName('img')[0].src='eyes/fearful.png'
+    }
+
+    }
+
+
+function flip2() {
+    var userinput = document.getElementById("answer-1").value;
+    if (userinput == "1"){
+      document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/1.png'
+    }
+    else if(userinput == "2"){
+      document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/2.png'
+    }
+    else if(userinput == "3"){
+        document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/3.png'
+      }
+    else if(userinput == "4"){
+        document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/4.png'
+    }
+    else if(userinput == "5"){
+        document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/5.png'
+    }
+    else if(userinput == "6"){
+        document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/6.png'
+    }
+    else if(userinput == "7"){
+        document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/7.png'
+    }
+    else if(userinput == "8"){
+        document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/8.png'
+    }
+    else if(userinput == "9"){
+        document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/9.png'
+    }
+    else if(userinput == "10"){
+        document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/10.png'
+    }
+    else if(userinput == "11"){
+        document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/11.png'
+    }
+    else if(userinput == "12"){
+        document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/12.png'
+    }
+    else if(userinput == "13"){
+        document.getElementById('resizable2').getElementsByTagName('img')[0].src='ear/13.png'
+    }
+    $('.card-2').toggleClass('flipped');
 }
 
 function flip3() {
@@ -205,11 +272,50 @@ function flip3() {
 
 function flip4() {
     var userinput = document.getElementById("answer-3").value;
-    if (userinput == "a"){
-      document.getElementById('resizable4').getElementsByTagName('img')[0].src='beautiful-island.jpg'
+    if (userinput == "1"){
+      document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/one.png'
     }
-    else if(userinput == "b"){
-      document.getElementById('resizable4').getElementsByTagName('img')[0].src='pagetwo_graph_two_2.png'
+    else if(userinput == "2"){
+      document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/two.png'
+    }
+    else if(userinput == "3"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/three.png'
+      }
+    else if(userinput == "4"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/four.png'
+    }
+    else if(userinput == "5"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/five.png'
+    }
+    else if(userinput == "6"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/six-2.png'
+    }
+    else if(userinput == "7"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/seven.png'
+    }
+    else if(userinput == "8"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/eight.png'
+    }
+    else if(userinput == "9"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/nine.png'
+    }
+    else if(userinput == "10"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/ten.png'
+    }
+    else if(userinput == "11"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/eleven.png'
+    }
+    else if(userinput == "12"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/twelve.png'
+    }
+    else if(userinput == "13"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/thirteen.png'
+    }
+    else if(userinput == "14"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/fourteen.png'
+    }
+    else if(userinput == "15"){
+        document.getElementById('resizable4').getElementsByTagName('img')[0].src='mouth/fifteen.png'
     }
     $('.card-4').toggleClass('flipped');
 }
